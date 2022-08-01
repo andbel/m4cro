39ca345c7b57013dcc52aaea5b506d814e87caa5
AB <ceo@winability.com>
2022-08-01 08:41:33 -0600

    Getting started

---
+++ m4cro.php
@@ -0,0 +1,14 @@
+<?php
+
+////////////////////////////
+// m4cro.php 
+//
+// Copyright 2022 Andrei Belogortseff. All rights reserved.
+//
+// Usage:
+// 
+// $ php m4cro.php input.txt output.txt
+//
+// see README.md for more info
+
+?>
